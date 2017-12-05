6b68e86aa75e720359580eb7445fa529f0f49370
Your Name <hashbyadamson@gmail.com>
2017-12-05 15:19:13 -0700

    Added Models Order-TestTube

---
+++ Intex/Intex/Models/Test_Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("Test_Result")]
+    public class Test_Result
+    {
+        [Key]
+        public int testTubeNumber { get; set; }
+
+        [ForeignKey("Order")]
+        public virtual int orderID { get; set; }
+        public virtual Order Order { get; set; }
+
+        [ForeignKey("Test")]
+        public virtual int testID { get; set; }
+        public virtual Test Test { get; set; }
+
+        [ForeignKey("Data_Report")]
+        public virtual int? dataReportID { get; set; }
+        public virtual Data_Report Data_Report { get; set; }
+
+    }
+}
---
+++ Intex/Intex/Models/Test_Tube.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+
+    [Table("Test_Tube")]
+    public class Test_Tube
+    {
+        [Key]
+        public int testTubeNumber { get; set; }
+        public int LTNumber{ get; set; }
+        public decimal concentration_mg { get; set; }
+
+        [ForeignKey("Test")]
+        public virtual int testID { get; set; }
+        public virtual Test Test { get; set; }
+
+    }
+}
