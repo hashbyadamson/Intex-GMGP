d7407d2fbf8349c2986236a9f26eeecf1e5f8501
Your Name <hashbyadamson@gmail.com>
2017-12-06 11:11:06 -0700

    Changes for different Controllers

--- Intex/Intex/Controllers/SingaporePharmController.cs
+++ Intex/Intex/Controllers/SingaporePharmController.cs
@@ -1,13 +1,40 @@
-﻿using System;
+﻿using Intex.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Net;
+using Intex.Models;
 
 namespace Intex.Controllers
 {
     public class SingaporePharmController : Controller
     {
+        public IntexContext db = new IntexContext();
+
+        public ActionResult ViewPastTests()
+        {
+            var test_Results = db.Test_Results.Include(t => t.Data_Report).Include(t => t.Order).Include(t => t.Test);
+            return View(test_Results.ToList());
+        }
+
+        public ActionResult PastTestDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Test_Result test_Result = db.Test_Results.Find(id);
+            if (test_Result == null)
+            {
+                return HttpNotFound();
+            }
+            return View(test_Result);
+        }
+
         // GET: SingaporePharm
         public ActionResult Index()
         {
@@ -39,10 +66,6 @@ namespace Intex.Controllers
             return View();
         }
 
-        public ActionResult ViewPastTests()
-        {
-            return View();
-        }
 
     }
 }
